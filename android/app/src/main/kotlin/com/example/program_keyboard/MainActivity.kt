54aa9d92789e09ed7eafd0c7e38031272d8e0966
zacksleo <zacksleo@gmail.com>
2024-01-07 20:24:41 +0800

    init commit

---
+++ android/app/src/main/kotlin/com/example/program_keyboard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.program_keyboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/keyboard/program_keyboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProgramKeyboardScreen extends StatefulWidget {
+  const ProgramKeyboardScreen({super.key});
+
+  @override
+  State<ProgramKeyboardScreen> createState() => _ProgramKeyboardScreenState();
+}
+
+class _ProgramKeyboardScreenState extends State<ProgramKeyboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('hello world');
+  }
+}
